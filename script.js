15268d93f8dd5eba47e4a681f88f1e72bc38a099
camilleWeb <cam_bochard@hotmail.fr>
2021-11-03 18:15:35 +0100

    Nouvelle intro premiere présentation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const removeRightBarButton = document.querySelector('#rigft-remove-btn');
 const rightNavBar = document.querySelector('#right-nav');
 const content = document.querySelector('.container');
 
-
+//********************RIGHT NAV BAR ANIM ********************** */
 rightBarButton.addEventListener('click',show);
 
 function show() {
@@ -16,6 +16,14 @@ function removeBar() {
     rightNavBar.classList.remove('active');
 }
 
+// ********************* GSAP ANIM **********************
+
+// Texte intro 
+
+var tl = gsap.timeline();
+tl.to(".title-Wel", {x:-1380, duration:1})
+.to(".pa1", {x:-1580, duration:1},"-=0.5")
+.to(".pa2", {x:-1180, duration:1},"-=0.5");
 
 // **************** SPACE PARALLAX ***************** //
 document.addEventListener("mousemove",parallax);
