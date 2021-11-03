cbe60f52cb57e0788aa9cd16f8cb9c1ab12e47ab
camilleWeb <cam_bochard@hotmail.fr>
2021-11-03 21:12:04 +0100

    Ajout section scroll anim avec la rocket

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollToPlugin, EasePack, ScrollTrigger, CSSRulePlugin, EaselPlugin, MotionPathPlugin);
 
 const rightBarButton = document.querySelector('#btn-rightNav');
 const removeRightBarButton = document.querySelector('#rigft-remove-btn');
@@ -16,7 +17,7 @@ function removeBar() {
     rightNavBar.classList.remove('active');
 }
 
-// ********************* GSAP ANIM **********************
+// ********************* GSAP ANIM Intro **********************
 
 // Texte intro 
 
@@ -24,6 +25,36 @@ var tl = gsap.timeline();
 tl.to(".title-Wel", {x:-1380, duration:1})
 .to(".pa1", {x:-1580, duration:1},"-=0.5")
 .to(".pa2", {x:-1180, duration:1},"-=0.5");
+
+/* ******************* Scroll Rocket Animation ***************** */
+var road = [
+    {x:0 , y: 0},
+    {x: 50, y: -50},
+    {x: 100, y: -100},
+    {x:500,y:-100},
+ /*    {x:100,y:100},
+    {x:200,y:250},
+    {x:500,y:350}, */
+];
+
+gsap.to('#marion', {
+    scrollTriger: {
+        trigger:"#marion",
+        markers: true,
+
+    },
+    motionPath: {
+        path: road,
+        curviness:1,
+        autoRotate: true
+    },
+    duration: 2,
+    ease: Power1.easeOut,
+});
+
+
+
+
 
 // **************** SPACE PARALLAX ***************** //
 document.addEventListener("mousemove",parallax);
